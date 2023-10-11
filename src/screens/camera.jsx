9ad2aba9c0dfd51e7980f7276ab4c3ded9f67036
Muhammed Zeba <parice02@hotmail.com>
2023-10-11 19:32:08 +0000

    add code par scanner

--- src/screens/camera.jsx
+++ src/screens/camera.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default function App() {
+export default function Camera() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>Cette application requiert l'utilisation de votre caméra.</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Pas de permission pour accéder à la caméra.</Text>;
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    if (type === 256) {
+      setScanned(true);
+      alert(`Bar code de type '${type}' et le contenu est: '${data}' ont été scanné avec succès!`);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Caméra</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={"Scanner"} onPress={() => setScanned(false)} />}
     </View>
   );
 }
